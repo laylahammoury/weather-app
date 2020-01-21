d1b7cc6bdeeac6d8bb1bc224abb7982baf6e9379
laylaHammoury <laylahammoury@gmail.com>
2020-01-21 02:01:21 +0200

    save work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DetailsComponent } from './pages/details/details.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchResultComponent } from './components/search-result/search-result.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'search', component: SearchResultComponent},
-  {path: 'daydetails/:id', component: DetailsComponent}
+  {path: 'daydetails/:id', component: DetailsComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
@@ -17,4 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DetailsComponent, SearchResultComponent]
+export const routingComponents = [DetailsComponent, SearchResultComponent, LoginComponent]
