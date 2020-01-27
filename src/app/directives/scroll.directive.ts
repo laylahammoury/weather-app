89e951b2844850f054b3a8354db9759c3ff252a4
laylaHammoury <laylahammoury@gmail.com>
2020-01-27 02:53:55 +0200

    tidy code

--- src/app/directives/scroll.directive.ts
+++ src/app/directives/scroll.directive.ts
@@ -26,33 +26,9 @@ export class ScrollDirective {
         console.log("scroll up")
         header.style.height ="50px" 
         header.style. transition=" height 300ms ease"; 
-        // header.style.padding = ""
           
       }
       this.lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }
-  
-  @HostListener('click') onClick(){
-      console.log("click test")
-  }
-  // @HostListener("scroll", ["$event.target"])
-  //       public onScroll(targetElement){
-  //         this.el.nativeElement.style.height = this.height+"px";
-  //         console.log("test scroll")
-  //       }
-    //  onListenerTriggered(targetElement): void {
-
-    //     // Calculate the scroll percentage
-    //     const percent = Math.round((event.srcElement.scrollTop / (event.srcElement.scrollHeight - event.srcElement.clientHeight)) * 100);
-
-    //     // Compare the new with old and only raise the event if values change
-    //     if(this.percentValue !== percent){
-          
-    //       // Update the percent value
-    //       this.percentValue = percent;
-
-    //       // Emit the event
-    //       this.onScroll.emit(percent);
-    //     }
-    // }
+ 
 }
